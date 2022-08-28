12840d395f55729a3fb604f2a64e188785f0cb9b
unknown <rod.lopes.oliveira@gmail.com>
2022-08-27 21:57:25 -0300

    criado o lançamento do gráfico de pizza

--- pygraf.py
+++ pygraf.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
@@ -9,17 +10,41 @@ arquivo_excel_exon = "cttos_exonerados.xlsx"
 # df_dia_1 = pd.read_excel(arquivo_excel, sheet_name="dia1",)
 df_exon = pd.read_excel(arquivo_excel_exon)
 
+#excluindo as linhas duplicadas e salvando no data frame; 
 df_exon_tratada = df_exon.drop_duplicates(subset="id_imovel", ignore_index=True)
 
-df_exon_tratada.to_excel("cttos_exon_trat.xlsx")
+#gráfico de pizza, contagem dos tipos de imóveis
+
+residencial = df_exon_tratada.loc[df_exon_tratada['tp_imovel'] == 'Residencial'].count()[0]
+comercial = df_exon_tratada.loc[df_exon_tratada['tp_imovel'] == 'Comercial'].count()[0]
+total_tipo_imovel = residencial + comercial
+residencial = residencial/total_tipo_imovel
+comercial = comercial/total_tipo_imovel
+label_pie = 'Residencial', 'Não residencial'
+sizes_pie = residencial, comercial
+explode = (0, 0.1)
+
+print(sizes_pie)
+print(label_pie)
+print(total_tipo_imovel)
 
 
-#lucro_dos_vendedores = df_dia_1.groupby(["Nome"]).sum()
+fig1, ax1 = plt.subplots()
 
-#relatorio = lucro_dos_vendedores.loc[:,"Valor"]
+ax1.pie(sizes_pie, explode=explode, labels = label_pie, autopct='%1.1f%%', shadow=True, startangle=50)
+ax1.axis('equal')
+plt.show()
 
-#print(relatorio)
+'''
+#criando o arquivo df_exon_tratada.to_excel("cttos_exon_trat.xlsx")
 
-#relatorio.plot(kind = 'bar')
-#plt.show()
 
+lucro_dos_vendedores = df_dia_1.groupby(["Nome"]).sum()
+
+relatorio = lucro_dos_vendedores.loc[:,"Valor"]
+
+print(relatorio)
+
+relatorio.plot(kind = 'bar')
+plt.show()
+'''
