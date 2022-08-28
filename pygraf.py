b6844457a30729ac7eac6a85d4b654b4d5b25cdb
unknown <rod.lopes.oliveira@gmail.com>
2022-08-28 12:37:53 -0300

    adicionado a criação do gráfico de n° de exonerações por ano

--- pygraf.py
+++ pygraf.py
@@ -1,13 +1,15 @@
 from cProfile import label
+from tokenize import Number
+from turtle import width
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
+import datetime 
 
 #pegando o nome da planilha, não precisa do endereço por estar na mesma página
 arquivo_excel = "Dia01.xlsx"
 arquivo_excel_exon = "cttos_exonerados.xlsx"
 
-# df_dia_1 = pd.read_excel(arquivo_excel, sheet_name="dia1",)
 df_exon = pd.read_excel(arquivo_excel_exon)
 
 #excluindo as linhas duplicadas e salvando no data frame; 
@@ -22,17 +24,46 @@ residencial = residencial/total_tipo_imovel
 comercial = comercial/total_tipo_imovel
 label_pie = 'Residencial', 'Não residencial'
 sizes_pie = residencial, comercial
-explode = (0, 0.1)
+explode = (0, 0.1) #variação da peça da pizza
 
-print(sizes_pie)
-print(label_pie)
-print(total_tipo_imovel)
-
+#Parte para a criação do gráfico
 
 fig1, ax1 = plt.subplots()
 
 ax1.pie(sizes_pie, explode=explode, labels = label_pie, autopct='%1.1f%%', shadow=True, startangle=50)
 ax1.axis('equal')
+plt.show()
+
+#trabalhando com as contagens de exonerações por ano
+
+df_data = df_exon_tratada['dt_exoneracao'].dt.year
+print(df_data)
+df_exon_tratada.insert(10,"ano_exo",df_data, True)
+
+#trabalhando com os anos e fazendo as contagens de contrato
+list_bar = ['2019','2020','2021','2022']
+x = np.arange(4)
+cont_ano = []
+
+ano_2019 = df_exon_tratada[df_exon_tratada['ano_exo'] == 2019].count()[0]
+ano_2020 = df_exon_tratada[df_exon_tratada['ano_exo'] == 2020].count()[0]
+ano_2021 = df_exon_tratada[df_exon_tratada['ano_exo'] == 2021].count()[0]
+ano_2022 = df_exon_tratada[df_exon_tratada['ano_exo'] == 2022].count()[0]
+cont_ano= [ano_2019, ano_2020, ano_2021, ano_2022]
+
+print(list_bar)
+print(cont_ano)
+print(x)
+
+#plotando o gráfico de barras do número de contratos por ano
+fig, ax = plt.subplots()
+
+p1 = ax.bar(x, cont_ano)
+ax.set_xticks(x,list_bar)
+ax.set_xlabel("Anos")
+ax.set_ylabel("N° de exonerações")
+ax.bar_label(p1, label_type='edge')
+
 plt.show()
 
 '''
